1f5b6c7d7c5b1ddee0bb7b6c14c77b8f5e1927f7
ilovepakpak <ilovepakpak@gmail.com>
2024-06-25 14:21:33 +0700

    Add project files.

---
+++ CS434SC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CS434SC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
